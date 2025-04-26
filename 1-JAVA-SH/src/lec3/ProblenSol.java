97c1fa5b41b2b105776bb7eef914e99e7dd1b9af
kudo07 <vknwl077@gmail.com>
2025-04-27 00:37:58 +0530

    Merge remote-tracking branch 'origin/main' into main

--- 1-JAVA-SH/src/lec3/ProblenSol.java
+++ 1-JAVA-SH/src/lec3/ProblenSol.java
@@ -1,7 +1,12 @@
 package lec3;
-import lec2.Solution;
+
+import lec2.Addtwo;
+//only public class can export impotr
 public class ProblenSol {
     public static void main(String[] args) {
-        Solution sol=new Solution();
+        // Solution sol=new Solution();
+        //only public class are importing
+        Addtwo lec=new Addtwo();
+
     }
 }
