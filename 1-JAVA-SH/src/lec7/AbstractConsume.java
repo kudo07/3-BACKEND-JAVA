ca89fa3dbd2614ac3dc7ac31beab73b464cc6df0
kudo07 <vknwl077@gmail.com>
2025-05-09 12:06:45 +0530

    Merge remote-tracking branch 'origin/main' into main

---
+++ 1-JAVA-SH/src/lec7/AbstractConsume.java
@@ -0,0 +1,14 @@
+package lec7;
+
+public class AbstractConsume extends Profession {
+    public void profession() {
+        System.out.println("hi iam in th eperson");
+    }
+
+    @Override
+    public int printO() {
+        
+        return 0;
+    }
+    //defination or implementation
+}
--- 1-JAVA-SH/src/lec7/Calculation.java
+++ 1-JAVA-SH/src/lec7/Calculation.java
@@ -1,18 +1,31 @@
 package lec7;
 
 public class Calculation {
-public int sum(int val1, int val2){
-    int total=val1+val2;
-    //doing some logger stuff
-    System.out.println("addition"+total);
-    return total;
-}
-public int getPriceOfPen(){
-    int capPrice=2;
-    int penBodyPrice=5;
-    int totalPenPrice=sum(capPrice,penBodyPrice);
-    return totalPenPrice;
-}
+    int a = 10;
+    static int b = 199;
 
+    //normal method
+    public int sum(int val1, int val2) {
+        int total = val1 + val2;
+        //doing some logger stuff
+        System.out.println("addition" + total);
+        return total;
+    }
 
+    //static method
+    public static int getPriceOfPen() {
+        //non static field 'a' cannot be referenced from the static context
+        b = 12;
+
+        //b is static
+        //a/*/**/*/=10;
+        int capPrice = 2;
+        int penBodyPrice = 5;
+//    non static variable and method cannot be reference here
+//    int totalPenPrice=sum(capPrice,penBodyPrice);
+        //non static method cannot be referenced from a static context;
+        int totalPenPrice = capPrice + penBodyPrice;
+        System.out.println("here access from the Calculation class");
+        return totalPenPrice;
+    }
 }
