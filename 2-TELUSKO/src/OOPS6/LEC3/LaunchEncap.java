d8fab32a25ea2769dcd0e4de5e535fe914255fb1
kudo07 <vknwl077@gmail.com>
2025-05-30 01:56:05 +0530

    Merge remote-tracking branch 'origin/main' into main

--- 2-TELUSKO/src/OOPS6/LEC3/LaunchEncap.java
+++ 2-TELUSKO/src/OOPS6/LEC3/LaunchEncap.java
@@ -1,6 +1,29 @@
 package OOPS6.LEC3;
 class Book{
-    
+   private int pageNum;
+   void disp(){
+      System.out.println("Java and inheritence present in pagenumber"+pageNum);
+   }
+   void setPageNum(int pgNo) {
+      pageNum = pgNo;
+   }
+//   to get the pagenumber from the private entity
+//   his sole purpose is to get the value
+
+   int getPageNum(){
+      return pageNum;
+   }
+//   method whose sole purpose is to set the value of pageNum
 }
 public class LaunchEncap {
+   public static void main(String[] args) {
+
+   Book book=new Book();
+//   private pageNum is not access here only access in the same class
+//   book.pageNum=4;
+//      if we want  to set the page number we cannot update directly but we call setPageNum
+      book.setPageNum(12);
+   book.disp();
+   }
+
 }
